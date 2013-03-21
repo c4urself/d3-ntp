fd66cabcd0716018efeb17203e1cc3268ebaa50a
Christian Verkerk <christianverkerk@gmail.com>
2013-03-21 16:45:04 -0700

    General cleanup :sparkle: Added the color and removal of circles

--- public/js/script.js
+++ public/js/script.js
@@ -46,83 +46,85 @@ window.ntp = window.ntp || {
         var that = this;
 
         this.projection = d3.geo.orthographic()
-            .scale(400)
-	    .translate([490, 400])
-	    .clipAngle(90)
-	    .rotate([73.74, -45.66])
+            .scale(300)
+            .translate([490, 400])
+            .clipAngle(90)
+            .rotate([73.74, -45.66])
 
-        this.path = d3.geo.path()
+            this.path = d3.geo.path()
             .projection(this.projection);
 
-        this.svg = d3.select('#body').append('svg:svg')
-            .attr('width', 980)
+        this.svg = d3.select('#map').append('svg:svg')
+            .attr('width', 800)
             .attr('height', 800)
             .on('mousedown', function (d) {that.mouseDown.apply(that);});
 
-        d3.json('world-countries.json', function (collection) {
+        d3.json('world-countries.json', function loadCountriesF(collection) {
             that.feature = that.svg.selectAll('path')
-                .data(collection.features)
-                .enter()
-                .append('svg:path')
-		.attr("d", that.path)
+            .data(collection.features)
+            .enter()
+            .append('svg:path')
+            .attr('d', that.path);
         });
 
         this.bindMouseEvents();
 
-        d3.select('select').on('change', function () {
+        d3.select('select').on('change', function selectTimeoutF() {
             that.timeout = this.value;
-            console.log(that.timeout);
         });
 
         ntp.listen();
     },
 
     listen: function listenF() {
-        var that = this;
-        var socket = io.connect('http://zeit.rcloran.net:8080/latlon');
+        var that = this,
+            socket = io.connect('http://zeit.rcloran.net:8080/latlon'),
+            addCountry = function addCountryF(countryName) {
+                var name = countryName.replace(/[\s,]/g, '-').toLowerCase(),
+                    count = that.countries[name],
+                    $container = $('#countries'),
+                    $div;
+
+                if (!count) {
+                    that.countries[name] = 0;
+                    $div = $('<div class="' + name + '">' + countryName + ': <span></span></div>');
+                    $container.append($div);
+                } else {
+                    $div = $('.' + name, $container);
+                }
+
+                count = that.countries[name] += 1;
+                $('span', $div).html(count);
+            },
+            drawCircle = function drawCircleF(d) {
+                var c = d3.geo.circle()
+                    .origin([d[1], d[0]])
+                    .angle(0.5);
+
+                var p = that.svg.append('path')
+                    .datum(c())
+                    .attr('class', 'points');
+
+                p.attr('stroke', 'red').attr('d', that.path);
+
+                setTimeout(function removeCircleF() {
+                    p.remove();
+                }, that.timeout);
+            };
+
         socket.on('latlon', function pingReceived(lat, lon, countryName) {
-
-            // draw circle for each hit
             drawCircle([lat, lon]);
-
-            // add country
-            //addCountry(countryName);
-
+            addCountry(countryName);
         });
+
         socket.on('qps', function qpsReceived(qps) {
-            document.getElementById("qps").innerHTML = qps;
+            document.getElementById('qps').innerHTML = qps;
         });
+
         socket.on('viewers', function viewersReceived(viewers) {
-            document.getElementById("viewers").innerHTML = viewers;
+            document.getElementById('viewers').innerHTML = viewers;
         });
 
-        var addCountry = function addCountryF(countryName) {
-            var name = countryName.replace(/[\s,]/g, '-').toLowerCase(),
-                count = that.countries[name],
-                $container = $('#countries'),
-                $div;
-
-            if (!count) {
-                that.countries[name] = 0;
-                $div = $('<div class="' + name + '">' + countryName + ': <span></span></div>');
-                $container.append($div);
-            } else {
-                $div = $('.' + name, $container);
-            }
-
-            count = that.countries[name] += 1;
-            $('span', $div).html(count);
-        };
-
-        var drawCircle = function drawCircleF(d) {
-		var c = d3.geo.circle()
-               		.origin([d[1], d[0]])
-			.angle(0.5);
-		that.svg.append("path")
-			.datum(c())
-			.attr("class", "points")
-			.attr("d", that.path);
-        };
     }
 };
 
